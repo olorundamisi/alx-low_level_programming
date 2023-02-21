a7d3f5311251d17cd1683706b14f9303707a7b53
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-02-21 12:55:13 -0800

    Functions and Nested Loops in C - First Batch

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void);
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 9; i++)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+	}
+}
