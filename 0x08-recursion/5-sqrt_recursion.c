6b47c0cc5654d6f29c520a88b23f5a7c0d14cb28
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-03-08 12:09:36 -0800

    Recursion in C

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+int natural_sqrt(int n, int i);
+int _sqrt_recursion(int n);
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ *
+ * @n: number
+ *
+ * Return: natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (natural_sqrt(n, 0));
+}
+
+
+/**
+ * natural_sqrt - Find natural square root.
+ *
+ * @n: number
+ * @i: factor to test
+ *
+ * Return: natural square root
+ */
+
+int natural_sqrt(int n, int i)
+{
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	if (i * i == n)
+		return (i);
+
+	if (i == n / 2)
+		return (-1);
+
+	return (natural_sqrt(n, i + 1));
+
+}
