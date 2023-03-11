b56393f53423c517c922c4611223c6e771971fcd
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-03-10 17:53:21 -0800

    Undid an injustice...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,7 +15,7 @@ int wildcmp(char *s1, char *s2)
 	/* Bases cases */
 	if (*s2 == '*' && s2[1] && *s1 == '\0')
 		return (0);
-	if (*s1 == '\0' && *s1 == *s2)
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
 	/* Recursive cases */
