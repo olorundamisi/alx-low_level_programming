647cc21d9c0457eb1da0562d79b3547870f29a5d
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-03-16 15:55:52 -0700

    More malloc() and free()

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "main.h"
+
+
+/**
+ * _calloc -  function that allocates memory for an array, using malloc.
+ *
+ * @nmemb: number of elements param
+ * @size: size param
+ *
+ * Return: pointer.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p  = malloc(size * nmemb);
+
+	if (p == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < (size * nmemb))
+	{
+		p[i] = 0;
+		i++;
+	}
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "main.h"
+
+
+/**
+ * array_range -  function that creates an array of integers.
+ *
+ * @min: minimum number param
+ * @max: maximum number param
+ *
+ * Return: pointer.
+ */
+
+int *array_range(int min, int max)
+{
+	if (min > max)
+		return (NULL);
+
+	int *p;
+	int i, j;
+
+	i = (max - min) + 1;
+	p = malloc(i *sizeof(*p));
+
+	if (p == NULL)
+		return (NULL);
+
+	j = 0;
+	while (j <= i && min <= max)
+	{
+		p[j] = min;
+		j++;
+		min++;
+	}
+
+	return (p);
+}
