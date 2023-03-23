7fd71b47d0c74ca630edbb332d66d4c73833daf5
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-03-23 12:47:04 -0700

    0x0E - struct and typedef in C

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "variadic_functions.h"
+
+
+/**
+ * print_numbers - print numbers
+ *
+ * @separator: the string to print between numbers
+ * @n: number of arguments
+ * @...: arguments
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(ap);
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "variadic_functions.h"
+
+
+/**
+ * print_strings - print strings
+ *
+ * @separator: separator
+ * @n: number of arguments
+ * @...: arguments
+ *
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *s;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(ap, char *);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s", s);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(ap);
+	printf("\n");
+}
