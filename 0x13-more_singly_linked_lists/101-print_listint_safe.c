9ad50ab97db38736085dc3dbbbd9271ab448a4bf
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-04-03 14:10:50 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,21 +8,21 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-		size_t i;
-		listint_t *b = (listint_t *)head;
+	size_t i;
+	listint_t *b = (listint_t *)head;
 
-		i = 0;
-		while (b && b > b->next)
-		{
-				printf("[%p] %d\n", (void *)b, b->n);
-				b = b->next;
-				i++;
-		}
-		if (b)
-		{
-				printf("[%p] %d\n", (void *)b, b->n);
-				printf("-> [%p] %d\n", (void *)b->next, b->next->n);
-				i++;
-		}
-		return (i);
+	i = 0;
+	while (b && b > b->next)
+	{
+		printf("[%p] %d\n", (void *)b, b->n);
+		b = b->next;
+		i++;
+	}
+	if (b)
+	{
+		printf("[%p] %d\n", (void *)b, b->n);
+		printf("-> [%p] %d\n", (void *)b->next, b->next->n);
+		i++;
+	}
+	return (i);
 }
