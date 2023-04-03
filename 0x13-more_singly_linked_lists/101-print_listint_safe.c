fee701fd2c2b75fdc1f632c051da7f0dfb4bb75b
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-04-03 14:24:37 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,51 @@
 #include "lists.h"
 
+
 /**
- * print_listint_safe - prints a listint_t list
+ * is_visited - assert if a node in a linked list is visited
+ * @node: pointer to node
+ * @visited: list of visited nodes
+ * @num: length of visited nodes
+ * Return: 1 if is visited else return 0
+ */
+
+int is_visited(const listint_t *node, const listint_t **visited, int num)
+{
+	int i = 0;
+
+	while (i < num)
+	{
+		if (node == visited[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+
+/**
+ * print_listint_safe - prints a listint_t list safely, even loops
  * @head: pointer to the first element of the list
  * Return: number of nodes
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i;
-	listint_t *b = (listint_t *)head;
+	int number_of_nodes = 0;
+	const listint_t *visited[1024];
 
-	i = 0;
-	while (b && b > b->next)
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)b, b->n);
-		b = b->next;
-		i++;
+		if (is_visited(head, visited, number_of_nodes))
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+		visited[number_of_nodes++] = head;
+
+		printf("[%p] %d\n", (void *)head, head->n);
+
+		head = head->next;
 	}
-	if (b)
-	{
-		printf("[%p] %d\n", (void *)b, b->n);
-		printf("-> [%p] %d\n", (void *)b->next, b->next->n);
-		i++;
-	}
-	return (i);
+	return (number_of_nodes);
 }
