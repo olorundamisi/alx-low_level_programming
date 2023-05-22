0a2defb9c890090af9ad9a4af86411ea6d0ba380
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-23 00:47:08 +0100

    gmli.so : Library Mask Injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libmask.so https://github.com/olorundamisi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp/ https://github.com/olorundamisi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinjection.so
+export LD_PRELOAD=/tmp/libinjection.so
