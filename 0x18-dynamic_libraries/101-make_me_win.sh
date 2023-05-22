e0461307aeaef34727b610d25702d0f0028d78f8
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-23 00:13:44 +0100

    ldpl.so : Code Injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -P /tmp https://github.com/olorundamisi/alx-low_level_programming/raw/main/0x18-dynamic_libraries/ld.so
-export LD_PRELOAD=/tmp/ld.so
+wget -P /tmp https://github.com/olorundamisi/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-make_me_win.sh
+export LD_PRELOAD=/tmp/ldpl.so
