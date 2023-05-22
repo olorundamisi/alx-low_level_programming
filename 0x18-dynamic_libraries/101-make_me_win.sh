3f754a56f37bce26696d78f7fc2ae6e2d83a1fdd
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-23 00:51:00 +0100

    gmli.so : convert injection to mask

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/olorundamisi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinjection.so
-export LD_PRELOAD=/tmp/libinjection.so
+wget -O /tmp/libmask.so https://github.com/olorundamisi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
